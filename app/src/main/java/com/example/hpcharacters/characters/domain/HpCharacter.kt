43fe61b6f5c27ae115fab804ccb78d1b5e793dbf
Michal Murin <mmurin.dev@gmail.com>
2023-06-22 18:18:56 +0200

    HP-3 make the HpCharacter parcelizable

--- app/src/main/java/com/example/hpcharacters/characters/domain/HpCharacter.kt
+++ app/src/main/java/com/example/hpcharacters/characters/domain/HpCharacter.kt
@@ -1,7 +1,11 @@
 package com.example.hpcharacters.characters.domain
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class HpCharacter(
     val name: String,
     val actor: String,
     val image: String,
-)
+): Parcelable
